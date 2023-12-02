5c81f994e7bafbd56dfefb4d8cba5aab19e38111
Jonathan Fernandes <jonathanfernzofficial@gmail.com>
2023-12-02 12:03:27 +0530

    radial progress bar issues fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,62 +7,33 @@ import {
 } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import ChangingProgressProvider from "./ChangingProgressProvider";
+import progress1 from '../assets/SVG/Asset 1.svg';
+import progress2 from '../assets/SVG/Asset 2.svg';
+import progress3 from '../assets/SVG/Asset 6.svg';
+import progress4 from '../assets/SVG/Asset 7.svg';
+import progress5 from '../assets/SVG/Asset 8.svg';
 
 export default function Hero() {
   return (
     <div className="hero">
       <div className="hero-item">
-        <CircularProgressbarWithChildren value={40} styles={buildStyles({
-            pathColor: "#CCFF00",
-            rotation: 0.5 + (1 - 30 / 100) / 2
-          })}>
-          <div style={{ fontSize: 30, marginTop: -5 }}>
-            <strong>300+</strong>
-          </div>
-        </CircularProgressbarWithChildren>
+        <img src={progress1} alt="" />
         <h3>Project Affiliated</h3>
       </div>
       <div className="hero-item">
-        <CircularProgressbarWithChildren value={90} styles={buildStyles({
-            pathColor: "#CCFF00",
-          })}>
-          <div style={{ fontSize: 30, marginTop: -5 }}>
-            <strong>INR 1200+</strong>
-          </div>
-        </CircularProgressbarWithChildren>
+        <img src={progress2} alt="" />
         <h3>Worth of Real Estate Sold</h3>
       </div>
       <div className="hero-item">
-        <CircularProgressbarWithChildren value={75} styles={buildStyles({
-            pathColor: "#CCFF00",
-            rotation: 0.5 + (1 - 80 / 100) / 2
-          })}>
-          <div style={{ fontSize: 30, marginTop: -5 }}>
-            <strong>800+</strong>
-          </div>
-        </CircularProgressbarWithChildren>
+        <img src={progress3} alt="" />
         <h3>Delighted Customers</h3>
       </div>
       <div className="hero-item">
-        <CircularProgressbarWithChildren value={30} styles={buildStyles({
-            pathColor: "#CCFF00",
-            rotation: 0.5 + (1 - 20 / 100) / 2
-          })}>
-          <div style={{ fontSize: 30, marginTop: -5 }}>
-            <strong>100+</strong>
-          </div>
-        </CircularProgressbarWithChildren>
+        <img src={progress4} alt="" />
         <h3>Professionals</h3>
       </div>
       <div className="hero-item">
-        <CircularProgressbarWithChildren value={20} styles={buildStyles({
-            pathColor: "#CCFF00",
-            rotation: 0.5 + (1 - 90 / 100) / 2
-          })}>
-          <div style={{ fontSize: 30, marginTop: -5 }}>
-            <strong>02</strong>
-          </div>
-        </CircularProgressbarWithChildren>
+        <img src={progress5} alt="" />
         <h3>Branch Offices</h3>
       </div>
       
