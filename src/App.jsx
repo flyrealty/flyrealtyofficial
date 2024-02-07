763bc16c951d9a39bd8b3d50acd809a4e23071e3
Chirag Kumar <chiragkrkashyap@gmail.com>
2024-02-07 18:30:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,46 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import {Projects, About, Connect, PrivacyPolicy, Insights, Home} from './exports';
-import NotFound from './components/NotFound';
-import Business from './components/Business';
-import Auth from './components/cms/Auth';
-import Dashboard from './components/cms/Dashboard';
-import whatsappIcon from './assets/whatsapp.png';
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  Projects,
+  About,
+  Connect,
+  PrivacyPolicy,
+  Insights,
+  Home,
+} from "./exports";
+import NotFound from "./components/NotFound";
+import Business from "./components/Business";
+import Auth from "./components/cms/Auth";
+import Dashboard from "./components/cms/Dashboard";
+import whatsappIcon from "./assets/whatsapp.png";
 
 export default function App() {
 
-   // Function to handle redirection to WhatsApp
-   const redirectToWhatsApp = () => {
-    // You can replace the phone number with the desired number
-    const phoneNumber = '9844473355';
-    window.open(`https://wa.me/${phoneNumber}`, '_blank');
+  const redirectToWhatsApp = () => {
+    
+    const phoneNumber = "9844473355";
+    window.open(`https://wa.me/${phoneNumber}`, "_blank");
   };
 
   return (
     <BrowserRouter>
-     
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/projects' element={<Projects />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="/insights" element={<NotFound />} />
         <Route path="/business" element={<Business />} />
         <Route path="/about" element={<About />} />
         <Route path="/connect" element={<Connect />} />
-        <Route path='/privacy' element={<PrivacyPolicy />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
         {/* <Route path='/auth' element={<Auth />} />
         <Route path='/dashboard' element={<Dashboard />} /> */}
-
-       
       </Routes>
+
       {/* WhatsApp Icon */}
-     <div className="whatsapp-icon" onClick={redirectToWhatsApp}>
-     <img src={whatsappIcon} alt="WhatsApp Icon" />
-   </div>
+      <div className="whatsapp-icon" onClick={redirectToWhatsApp}>
+        <img src={whatsappIcon} alt="WhatsApp Icon" />
+      </div>
+      
     </BrowserRouter>
-  ) 
+  );
 }
